bf098b759dfcad071346797a9feaae592c7bd0f5
yuu5m77@gmail.com <yuu5m77@gmail.com>
2024-10-25 20:49:58 +0900

    削除

---
+++ laravel-samuraimart/app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function favorited_users() {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+}
